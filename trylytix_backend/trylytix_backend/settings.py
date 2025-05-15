847a6d6ac532f733e37ff830620308e0f0374a58
ThevinRahu50 <thevin@jobnet.nz>
2025-05-15 16:07:04 +1200

    fixes to eb

--- trylytix_backend/trylytix_backend/settings.py
+++ trylytix_backend/trylytix_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0df3qv5p4i70nv5n@i=@25+m&%yrj@p*&nxz*w1j%)*)o1^$6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['trylytixenv.eba-2sfsrbzp.ap-southeast-2.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
--- trylytix_backend/trylytix_backend/urls.py
+++ trylytix_backend/trylytix_backend/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.http import JsonResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("OK")),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
